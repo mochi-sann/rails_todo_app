7d13e467468805e96cad418862f84e3fc9aa5968
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-03-08 23:30:28 +0900

    フォーマットしたりした

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,4 @@
 class TodoController < ApplicationController
-
-
   # def edit
   #   @todo = Todo.find(params[:id])
   # end
@@ -11,14 +9,27 @@ class TodoController < ApplicationController
   #   redirect_to todo_index_path
   # end
   def index
-    @todo = Todo.all
+    @todos = Todo.all
   end
   def show
     @todo = Todo.find(params[:id])
   end
   def new
     @todo = Todo.new(title: params[:title], done: false)
+    print @todo
     # @todo.save
-    # redirect_to todo_path(@todo)
+  end
+  def create
+  @todo = Todo.new(title: params[:title], done: false)
+    @todo = Todo.new(todo_params)
+    print @todo
+    if @todo.save
+      redirect_to root_path, notice: "Todo was successfully created." # 保存成功時
+    else
+      render :new  # 保存失敗時（バリデーションエラーなど）はnewテンプレートを再表示
+    end
+  end
+  private def todo_params
+    params.require(:todo).permit(:title) # ストロングパラメーター
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1, maximum: 100 }
-  validates :done, inclusion: { in: [true, false] }
+ attribute :done, :boolean, default: false
 end
