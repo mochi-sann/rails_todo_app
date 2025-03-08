8b1ae59853c97fe21bbeaec14921c98cc93c75a3
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-03-08 22:47:55 +0900

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,17 +1,24 @@
 class TodoController < ApplicationController
-  def show
+
+
+  def edit
     @todo = Todo.find(params[:id])
   end
 
-  def create
-  end
-
-  def edit
-  end
-
   def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todo_index_path
   end
   def index
     @todo = Todo.all
+  end
+  def show
+    @todo = Todo.find(params[:id])
+  end
+  def new
+    @todo = Todo.new(title: params[:title], done: false)
+    @todo.save
+    redirect_to todo_path(@todo)
   end
 end
