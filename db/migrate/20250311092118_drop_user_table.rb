cd42d87f9dfaf17996834afe19c1509cb9cf2e48
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-03-11 18:22:33 +0900

    削除(migrate): usersテーブルを削除するマイグレーションを追加 修正(schema.rb): スキーマのバージョンを更新し、usersテーブルを削除

---
+++ db/migrate/20250311092118_drop_user_table.rb
@@ -0,0 +1,5 @@
+class DropUserTable < ActiveRecord::Migration[8.0]
+  def change
+    drop_table :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_08_135553) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_11_092118) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -23,14 +23,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_08_135553) do
     t.boolean "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "password_digest", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
