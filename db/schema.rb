74d8ad65e3f8d4e59e8cc0770adb32d9946f39da
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-03-15 12:08:10 +0900

    update user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_11_101735) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_15_030147) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -70,6 +70,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_101735) do
     t.boolean "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -83,4 +85,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_11_101735) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "sessions", "users"
+  add_foreign_key "todos", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-10.times do |i|
-  Todo.find_or_create_by!(title: "Sample Post #{i + 1}", done: rand(2) == 1)
-end
 
 User.find_or_create_by!(email_address: "dev_user1@example.com") do |user|
   user.password = "password"
@@ -27,4 +24,7 @@ User.find_or_create_by!(email_address: "dev_user3@example.com") do |user|
   user.password = "password"
   user.password_confirmation = "password"
   # user.name = "開発ユーザー3"
+end
+10.times do |i|
+  Todo.find_or_create_by!(title: "Sample Post #{i + 1} dev_user1", done: rand(2) == 1, user_id: 1)
 end
